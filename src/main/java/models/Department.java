2170182a6aa032123423e00e26048b158b2e599a
Louis-crypto <kinglouis745@gmail.com>
2020-02-17 13:01:18 +0300

    create a constructor for the department class

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,8 +1,14 @@
 package models;
 
 public class Department {
-    private String name;
+    private String Deptname;
     private String description;
     private int noOfEmployees;
     private int id;
+
+    public Department(String Deptname, String description, int noOfEmployees){
+        this.Deptname = Deptname;
+        this.description = description;
+        this.noOfEmployees = noOfEmployees;
+    }
 }
