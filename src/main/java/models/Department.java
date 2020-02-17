f5f5587722d6e5f46d1843297d7dd470fd95d2b8
Louis-crypto <kinglouis745@gmail.com>
2020-02-17 13:30:52 +0300

    add equals and hashcode for the Department class

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,10 +1,29 @@
 package models;
 
+import java.util.Objects;
+
 public class Department {
     private String Deptname;
     private String description;
     private int noOfEmployees;
     private int id;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return noOfEmployees == that.noOfEmployees &&
+                id == that.id &&
+                Objects.equals(Deptname, that.Deptname) &&
+                Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Deptname, description, noOfEmployees, id);
+    }
 
     public Department(String Deptname, String description, int noOfEmployees){
         this.Deptname = Deptname;
